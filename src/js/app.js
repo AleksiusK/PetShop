f19acd015419200bd02dd4863cb02815da187607
AleksiusK <kurkela.aleksius@gmail.com>
2021-04-23 19:27:22 +0300

    attempting to fix button issue, adopt.json does not load

--- src/js/app.js
+++ src/js/app.js
@@ -49,7 +49,7 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('adoption.json', function(data) {
+    $.getJSON('./adoption.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with truffle-contract
       var adoptionArtifact = data;
       App.contracts.adoption = TruffleContract(adoptionArtifact);
